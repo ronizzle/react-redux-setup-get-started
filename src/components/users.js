fa37dd32a14a77e262bc31e8ae189e6ae77ab74e
ronizzle <roniel44@gmail.com>
2017-04-09 00:30:51 +0800

    Learned about simply adding links on react components

--- src/components/users.js
+++ src/components/users.js
@@ -2,6 +2,30 @@ import React from 'react';
 import Button from './button'
 import UserItem from './user-item'
 
+class Link extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.changeUrl = this.changeUrl.bind(this)
+  }
+
+  changeUrl() {
+      window.location.replace(this.props.newUrl)
+  }
+
+  render() {
+    return(
+      <div>
+        <Button
+          onClick={this.changeUrl}
+          buttonLabel={`Go to ${this.props.newUrl}`}
+          className='btn btn-blue' />
+        {this.props.children}
+      </div>
+    )
+  }
+}
+
 class Users extends React.Component {
   constructor(props) {
     super(props)
@@ -12,6 +36,9 @@ class Users extends React.Component {
     return (
       <div className='container'>
         <Button onClick={fetchUsers} buttonLabel='Fetch Users' className='btn btn-blue' />
+        <Link newUrl='twitter.com/eternalenvy1991'>
+          <p>Envy we won</p>
+        </Link>
         <div id="users">
           {data.users.map((user, i) => {
                 return (
